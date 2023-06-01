a37bb0ae04e4685511d0434920bf2a5b950e0f5b
billp <billpiazzetta@gmail.com>
2023-05-31 22:34:41 -0300

    Inseri botão, criei função handleChange, comecei a implementar a chamadado ao API

--- src/components/SearchAndLocation.jsx
+++ src/components/SearchAndLocation.jsx
@@ -1,16 +1,41 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 
 export default function SearchAndLocation(props) {
     const [cidade, setCidade] = useState("Rio de Janeiro");
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const APIKEY = '9d2e9a1845d97f44e0c3c9b98a003a63';
 
     const handleChange = (event) =>  { event.preventDefault();
         setCidade(event.target.value);
     };
 
-    return (
-        <input type="text" id="search"
-        onChange={handleChange} placeholder="Buscar cidade"
-        value={cidade} />
+    const handleSearch = () => {
+        fetch(apiCall)
+    }
 
+    var apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${APIKEY}`
+
+    return (
+        <div style={styles.container}>
+            <input 
+                type="text" 
+                id="search"
+                onFocus={() => setCidade("")}
+                onChange={handleChange} placeholder="Buscar cidade"
+                value={cidade} 
+            />
+            <button 
+                onClick={handleSearch}
+            >Buscar</button>
+        </div>
     )
+}
+
+const styles = {
+    container: {
+        marginTop: 100
+    }
 }
