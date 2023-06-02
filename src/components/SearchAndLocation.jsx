98b778a5c0c61deda49fd2c31d68f4c8c2d1b58b
billp <billpiazzetta@gmail.com>
2023-06-02 17:07:08 -0300

    Chamada de API funcionando

--- src/components/SearchAndLocation.jsx
+++ src/components/SearchAndLocation.jsx
@@ -3,38 +3,35 @@ import React, { useEffect, useState } from 'react';
 export default function SearchAndLocation(props) {
     const [cidade, setCidade] = useState("london");
     const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
+    // const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     const APIKEY = '9d2e9a1845d97f44e0c3c9b98a003a63';
 
-    // var apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${APIKEY}`
-
-    useEffect(() => {
-        const apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${APIKEY}`;
-      
-        fetch(apiCall)
-          .then((res) => res.json())
-          .then((usefulData) => {
-            console.log(usefulData);
-            setLoading(false);
-            setData(usefulData);
-          })
-          .catch((e) => {
-            setError(`An error occurred: ${e}`);
-            setLoading(false);
-          });
-      }, [cidade]);
-      
-
-    const handleChange = (event) =>  { event.           preventDefault();
-        setCidade(event.target.value);
-    };
+    const apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${APIKEY}`;
 
     const handleSearch = () => {
-        setCidade(cidade);
+        fetch(apiCall)
+            .then((result) => result.json())
+            .then((usefulData) => {
+                // console.log(usefulData);
+                // setLoading(false);
+                setData(usefulData);
+            })
+            .catch((e) => {
+                setError(`An error occurred: ${e}`);
+                // setLoading(false);
+            });
         console.log(data);
-    }
+    };
+
+    useEffect(() => {        
+        handleSearch();
+    }, []);
+
+    const handleChange = (event) =>  {event.preventDefault();
+        setCidade(event.target.value);
+    };
 
     // return (
     //     <>
@@ -51,7 +48,8 @@ export default function SearchAndLocation(props) {
                 type="text" 
                 id="search"
                 onFocus={() => setCidade("")}
-                onChange={handleChange} placeholder="Buscar cidade"
+                onChange={handleChange} 
+                placeholder="Buscar cidade"
                 value={cidade} 
             />
             <button 
